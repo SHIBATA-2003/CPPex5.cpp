0f278374f1ca43ff4789910043b22c45f13595d2
SHIBATA <ookinakurino3@gmail.com>
2023-01-11 09:42:56 +0900

    j

--- CPPex5.cpp/getVolumeSurface.cpp
+++ CPPex5.cpp/getVolumeSurface.cpp
@@ -1,2 +1,57 @@
 #include "getVolumeSurface.h"
-//
+struct BOX {	
+	double x;	
+	double y;	
+	double z;	
+	double volume;	
+	double surface; 
+	double packSize;
+};
+
+void getVolumeSurface(BOX* box) {
+	box->volume = box->x * box->y * box->z;
+	box->surface = 2 * (box->x * box->y + box->x * box->z + box->y * box->z);
+	return;
+}
+
+bool isSendable(BOX* box)
+{
+	double BoxSize = box->x + box->y + box->z;
+	if (BoxSize < 60)
+	{
+		box->packSize = 60;
+	}
+	else if (BoxSize < 80)
+	{
+		box->packSize = 80;
+	}
+	else if (BoxSize < 100)
+	{
+		box->packSize = 100;
+	}
+	else if (BoxSize < 120)
+	{
+		box->packSize = 120;
+	}
+	else if (BoxSize < 140)
+	{
+		box->packSize = 140;
+	}
+	else if (BoxSize < 160)
+	{
+		box->packSize = 160;
+	}
+	else if (BoxSize < 180)
+	{
+		box->packSize = 180;
+	}
+	else if (BoxSize < 200)
+	{
+		box->packSize = 200;
+	}
+	else
+	{
+		return false;
+	}
+	return true;
+}
--- CPPex5.cpp/getVolumeSurface.h
+++ CPPex5.cpp/getVolumeSurface.h
@@ -1,5 +1,12 @@
 #pragma once
-class getVolumeSurface
-{
+struct BOX {
+	double x;
+	double y;
+	double z;
+	double volume;
+	double surface;
+	double packSize;
 };
+void getVolumeSurface(BOX* box);
+bool isSendable(BOX* box);
 
